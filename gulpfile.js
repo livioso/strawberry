9dad734722f0d903a21db3f88f5766bb493a1e87
Livio Bieri <livio@livio.li>
2015-03-01 00:01:04 +0100

    Gulp task for jshint added

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
 var gulp = require('gulp');
 var runSeq = require('run-sequence');
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
 
 gulp.task('heroku:production', function(){
   // runSeq('clean', 'build', 'minify');
 });
 
+gulp.task('jshint', function() {
+  return gulp.src(['**/*.js', '!node_modules/**/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish));
+});
