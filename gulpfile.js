58350baefc8a94d4f7e5095814833618bb30c3d1
Livio Bieri <livio@livio.li>
2015-02-28 22:15:36 +0100

    empty herokutask for now.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,6 @@ var gulp = require('gulp');
 var runSeq = require('run-sequence');
 
 gulp.task('heroku:production', function(){
-  runSeq('clean', 'build', 'minify');
+  // runSeq('clean', 'build', 'minify');
 });
 
