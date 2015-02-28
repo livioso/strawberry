76d2ef1472bd91f04becdf6bb7dd26de32dc9287
Livio Bieri <livio@livio.li>
2015-03-01 00:15:51 +0100

    Gulp sass task added

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,25 @@ var gulp = require('gulp');
 var runSeq = require('run-sequence');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
+var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
 
 gulp.task('heroku:production', function(){
-  // runSeq('clean', 'build', 'minify');
+  runSeq('sass');
 });
 
 gulp.task('jshint', function() {
   return gulp.src(['**/*.js', '!node_modules/**/*.js'])
     .pipe(jshint())
     .pipe(jshint.reporter(stylish));
+});
+
+gulp.task('sass', function(done) {
+  gulp.src('./scss/main.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./public'))
+    .pipe(minifyCss({
+      keepSpecialComments: 0
+    })).on('end', done);
 });
