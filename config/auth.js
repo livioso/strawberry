1f0d08f47642e4079e690f5f9f57cc74720d2145
Livio Bieri <livio@livio.li>
2015-03-16 23:08:40 +0100

    Indentation fixed

--- config/auth.js
+++ config/auth.js
@@ -2,23 +2,22 @@ var LocalStrategy = require('passport-local').Strategy;
 
 module.exports = function(passport) {
 
-	passport.use(new LocalStrategy(
-		function(username, password, done) {
+  passport.use(
+    new LocalStrategy(
+      function(username, password, done) {
+        if (username === process.env.DEVUSERNAME && password === process.env.DEVUSERPW) {
+          return done(null, {name: "DEVUSER"});
+        }
 
-			if (username === process.env.DEVUSERNAME && password === process.env.DEVUSERPW) {
-				return done(null, {name: "DEVUSER"});
-			}
+        return done(null, false, { message: 'Incorrect username.' });
+      })
+    );
 
-			return done(null, false, { message: 'Incorrect username.' });
-		}
-	));
+  passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
 
-	passport.serializeUser(function(user, done) {
-		done(null, user);
-	});
-
-	passport.deserializeUser(function(user, done) {
-		done(null, user);
-	});
-
+  passport.deserializeUser(function(user, done) {
+    done(null, user);
+  });
 };
