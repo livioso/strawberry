33d3f82ef7a5b9e703656c998ba48d7bf9e18525
Livio Bieri <livio@livio.li>
2015-03-17 13:23:47 +0100

    Indentation fixed

--- app/routes.js
+++ app/routes.js
@@ -6,140 +6,137 @@ var Presets = require('./data/presets');
 // middleware function to be used
 // for every secured routes
 var auth = function(req, res, next) {
-	if (!req.isAuthenticated()) {
+  if (!req.isAuthenticated()) {
     res.sendStatus(401);
-	}
-	else {
-		next();
-	}
+  } else {
+    next();
+  }
 };
 
 
 function getShoppinglists(res) {
-	Shoppinglist.find(function(err, lists) {
-
-		if (err) {
-			res.send(err);
-		}
-
-		res.json(lists);
-	});
+  Shoppinglist.find(function(err, lists) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json(lists);
+    }
+  });
 }
 
 function getItems(res) {
-	Item.find(function(err, items) {
-
-		if (err) {
-			res.send(err);
-		}
-
-		res.json(items);
-	});
+  Item.find(function(err, items) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json(items);
+    }
+  });
 }
 
 // --- POST / GET ---
 module.exports = function(app, passport) {
 
-	// get all shoppinglists
-	// FIXME see /api/item
-	app.get('/api/items', function(req, res) {
+  // get all shoppinglists
+  // FIXME see /api/item
+  app.get('/api/items', function(req, res) {
 
-		// from mongo db -> via mongoose
-		getItems(res);
-	});
+    // from mongo db -> via mongoose
+    getItems(res);
+  });
 
-	app.put('/api/item/:id', function(req, res) {
+  app.put('/api/item/:id', function(req, res) {
 
-		Item.findOneAndUpdate(
-			{ _id: req.params.id },
-			{ $set: { itemText: req.body.itemText, itemChecked: req.body.itemChecked }}, {upsert: true}, function(err, doc){
-				if(err) {
-					return res.send(500, { error: err });
-				}
+    Item.findOneAndUpdate(
+      { _id: req.params.id },
+      { $set: { itemText: req.body.itemText, itemChecked: req.body.itemChecked }}, {upsert: true}, function(err, doc){
+        if(err) {
+          return res.send(500, { error: err });
+        }
 
-			return res.send("succesfully saved");
-		});
+      return res.send("succesfully saved");
+    });
 
-	});
-	// FIXME make me use middleware auth again :-(
-	app.post('/api/item', function(req, res) {
+  });
+  // FIXME make me use middleware auth again :-(
+  app.post('/api/item', function(req, res) {
 
-		new Item(req.body).save( function ( err, todo, count ){
-			if(err) return next( err );
-			res.redirect( '/' );
-		});
-	});
+    new Item(req.body).save( function ( err, todo, count ){
+      if(err) return next( err );
+      res.redirect( '/' );
+    });
+  });
 
 
-	app.put('/api/list/:id', function(req, res) {
-			Shoppinglist.findOneAndUpdate(
-			{ _id: req.params.id },
-			{ $set: { name: req.body.name},
-				$addToSet: { items: req.body.items}
-			}, {upsert: true}, function(err, doc){
-				if(err) {
-					return res.send(500, { error: err });
-				}
+  app.put('/api/list/:id', function(req, res) {
+      Shoppinglist.findOneAndUpdate(
+      { _id: req.params.id },
+      { $set: { name: req.body.name},
+        $addToSet: { items: req.body.items}
+      }, {upsert: true}, function(err, doc){
+        if(err) {
+          return res.send(500, { error: err });
+        }
 
-			return res.send("succesfully saved");
-		});
+      return res.send("succesfully saved");
+    });
 
-	});
+  });
 
-	app.get('/api/list/:idlist/:iditem', function(req, res) {
-		Shoppinglist.aggregate([
-			{$match:{_id: req.params.idlist}},
-			{$unwind:"$items"},
-			{$match:{"items._id": req.params.iditem}}
-		], function(err, items) {
-			if(err) {
-				res.send(500, {error: err});
-			} else {
-				res.json(items);
-			}
-		});
-	});
+  app.get('/api/list/:idlist/:iditem', function(req, res) {
+    Shoppinglist.aggregate([
+      {$match:{_id: req.params.idlist}},
+      {$unwind:"$items"},
+      {$match:{"items._id": req.params.iditem}}
+    ], function(err, items) {
+      if(err) {
+        res.send(500, {error: err});
+      } else {
+        res.json(items);
+      }
+    });
+  });
 
-	app.post('/api/list', function(req, res) {
+  app.post('/api/list', function(req, res) {
 
-		new Shoppinglist(req.body).save( function ( err, list, count ){
-			if(err) return next( err );
-			res.redirect( '/' );
-		});
-	});
+    new Shoppinglist(req.body).save( function ( err, list, count ){
+      if(err) return next( err );
+      res.redirect( '/' );
+    });
+  });
 
-	app.get('/api/list', function(req, res) {
-		getShoppinglists(res);
-	});
+  app.get('/api/list', function(req, res) {
+    getShoppinglists(res);
+  });
 
-	app.get('/loggedin', function(req, res) {
-		// fixme: this is a bit hacky
-		res.send(req.isAuthenticated() ? req.user : '0');
-	});
+  app.get('/loggedin', function(req, res) {
+    // fixme: this is a bit hacky
+    res.send(req.isAuthenticated() ? req.user : '0');
+  });
 
-	app.post('/login', passport.authenticate('local'), function(req, res) {
-		res.send(req.user);
-	});
+  app.post('/login', passport.authenticate('local'), function(req, res) {
+    res.send(req.user);
+  });
 
-	app.post('/logout', function(req, res) {
+  app.post('/logout', function(req, res) {
 
-		req.logOut();
-		// necessary, otherwise we still
-		// look like logged in (why is that?)
-		res.sendStatus(404);
-	});
+    req.logOut();
+    // necessary, otherwise we still
+    // look like logged in (why is that?)
+    res.sendStatus(404);
+  });
 
-	app.get('/category', auth, function(req, res) {
+  app.get('/category', auth, function(req, res) {
 
-		var products = Presets.categories.map(function(item) {
-			return item.product;
-		});
+    var products = Presets.categories.map(function(item) {
+      return item.product;
+    });
 
-		res.json(products);
-	});
+    res.json(products);
+  });
 
-	// -- application --
-	app.get('/', function(req, res) {
-		res.sendfile('./public/index.html'); // load the single view file
-	});
+  // -- application --
+  app.get('/', function(req, res) {
+    res.sendfile('./public/index.html'); // load the single view file
+  });
 };
