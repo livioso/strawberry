ba03e2049bf533faf7e557673fce9d4eb7d2181a
Livio Bieri <livio@livio.li>
2015-04-07 22:37:44 +0200

    Merged typeAheadController with shoppingListController

--- public/js/controllers/shoppingListController.js
+++ public/js/controllers/shoppingListController.js
@@ -2,8 +2,14 @@ angular.module('strawberry')
 .controller('shoppingListController', [
   '$scope',
   '$http',
-  function ($scope) {
+  function ($scope, $http) {
     'use strict';
+
+    // set the type ahead data
+    $http.get('/category').success(function (data) {
+      $scope.products = data;
+    }).error(function () {
+    });
 
     $scope.loading = true;
 
@@ -11,5 +17,8 @@ angular.module('strawberry')
         {'name': 'Maria und Livio'},
         {'name': 'At my parents'}
       ];
+
+    $scope.createShoppingItem = function () {
+    };
   }
 ]);
--- public/js/controllers/typeAheadController.js
+++
@@ -1,8 +0,0 @@
-angular.module('strawberry')
-.controller('TypeaheadCtrl', function ($scope, $http) {
-  'use strict';
-  $http.get('/category').success(function (data) {
-    $scope.products = data;
-  }).error(function () {
-  });
-});
