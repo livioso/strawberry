868356fc35e3e5ff72e95aaf23dfe78ba93e33c1
Livio Bieri <livio@livio.li>
2015-04-22 09:13:09 +0200

    New items construction creation bug fixed

--- public/js/services/shoppingListService.js
+++ public/js/services/shoppingListService.js
@@ -9,9 +9,9 @@ angular.module('shoppingListService', [])
       },
       create: function (shoppinglistdata) {
 
-        var json = {'items': [
+        var json = {'items':
           {'name': shoppinglistdata, 'checked': false}
-        ]};
+        };
 
         return $http.put('/api/list/55081de2162072120758fc53', json);
       },
