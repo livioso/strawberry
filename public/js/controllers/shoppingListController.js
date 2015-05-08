013b9f8c2a9525ca0252b049d3d9200354d376bd
Livio Bieri <livio@livio.li>
2015-05-08 11:29:39 +0200

    Minor refactoring of shoppingListController

--- public/js/controllers/shoppingListController.js
+++ public/js/controllers/shoppingListController.js
@@ -6,28 +6,17 @@ angular.module('strawberry')
   function ($scope, $http, ShoppingList) {
     'use strict';
 
-    $scope.formData = {};
-
     // set the type ahead data
     $http.get('/category').success(function (data) {
       $scope.products = data;
     });
 
-    $scope.loading = true;
-
     $scope.loadData = function() {
       ShoppingList.get().success(function (data) {
-        console.log('Working on it');
+        console.log(data[0].items);
         $scope.shoppinglistItems = data[0].items;
       });
     };
-
-    $scope.loadData();
-
-    $scope.shoppinglists = [
-        {'name': 'Maria und Livio'},
-        {'name': 'At my parents'}
-      ];
 
     $scope.createShoppingItem = function () {
       var item = $scope.formData.text;
@@ -35,8 +24,18 @@ angular.module('strawberry')
         ShoppingList.create(item).success(function () {
           $scope.loadData();
         });
+        // read for next entry
         $scope.formData = {};
       }
     };
+
+    // setup the data :)
+    $scope.formData = {};
+    $scope.loading = true;
+    $scope.loadData();
+    $scope.shoppinglists = [
+      {'name': 'Maria und Livio'},
+      {'name': 'At my parents'}
+    ];
   }
 ]);
